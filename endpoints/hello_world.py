67ab2a429979a3a92de9752f05cb4f1710ddf9f4
comfuture <comfuture@gmail.com>
2022-07-19 08:56:50 +0000

    add pytest.ini with doctest options

--- endpoints/hello_world.py
+++ endpoints/hello_world.py
@@ -7,4 +7,18 @@ router = APIRouter()
 
 @router.get("/hello-world")
 def hello():
+    """hello world를 리턴합니다
+
+    >>> hello()
+    'Hello World!'
+    """
     return "Hello World!"
+
+
+def add_numbers(a: int, b: int) -> int:
+    """숫자를 더합니다
+
+    >>> add_numbers(1, 2)
+    3
+    """
+    return a + b
